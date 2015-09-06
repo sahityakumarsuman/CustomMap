1b04eccdd545a2609a04da610b38485984c02541
sahityakumarsuman <sahityakumarsuman@gmail.com>
2015-09-07 01:59:40 +0530

    Initial commit

---
+++ SF Vehicles/src/main/java/com/firebase/custommap/MainActivity.java
@@ -0,0 +1,165 @@
+package com.firebase.custommap;
+
+import android.app.AlertDialog;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.support.v4.app.FragmentActivity;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Interpolator;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
+import com.firebase.geofire.GeoQuery;
+import com.firebase.geofire.GeoQueryEventListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends FragmentActivity implements GeoQueryEventListener, GoogleMap.OnCameraChangeListener {
+
+    private static final GeoLocation INITIAL_CENTER = new GeoLocation(37.7789, -122.4017); // custom fixed locaiton right now
+    private static final int INITIAL_ZOOM_LEVEL = 14;
+    // firebase server link for free acces of geo  
+    private static final String GEO_FIRE_REF = "https://publicdata-transit.firebaseio.com/_geofire";
+
+    private GoogleMap map;
+    private Circle searchCircle;
+    private GeoFire geoFire;
+    private GeoQuery geoQuery;
+
+    private Map<String,Marker> markers;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        this.map = mapFragment.getMap();
+        LatLng latLngCenter = new LatLng(INITIAL_CENTER.latitude, INITIAL_CENTER.longitude);
+        this.searchCircle = this.map.addCircle(new CircleOptions().center(latLngCenter).radius(1000));
+        this.searchCircle.setFillColor(Color.argb(66, 255, 0, 255));
+        this.searchCircle.setStrokeColor(Color.argb(66, 0, 0, 0));
+        this.map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLngCenter, INITIAL_ZOOM_LEVEL));
+        this.map.setOnCameraChangeListener(this);
+
+        Firebase.setAndroidContext(this);
+
+
+        this.geoFire = new GeoFire(new Firebase(GEO_FIRE_REF));
+
+        this.geoQuery = this.geoFire.queryAtLocation(INITIAL_CENTER, 1);
+
+        // setup markers -- baad me change kr skty h isko kis bi shape me
+        this.markers = new HashMap<String, Marker>();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        this.geoQuery.removeAllListeners();
+        for (Marker marker: this.markers.values()) {
+            marker.remove();
+        }
+        this.markers.clear();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        this.geoQuery.addGeoQueryEventListener(this);
+    }
+
+    @Override
+    public void onKeyEntered(String key, GeoLocation location) {
+        // Add a new marker to the map
+        Marker marker = this.map.addMarker(new MarkerOptions().position(new LatLng(location.latitude, location.longitude)));
+        this.markers.put(key, marker);
+    }
+
+    @Override
+    public void onKeyExited(String key) {
+        // Remove any old marker
+        Marker marker = this.markers.get(key);
+        if (marker != null) {
+            marker.remove();
+            this.markers.remove(key);
+        }
+    }
+
+    @Override
+    public void onKeyMoved(String key, GeoLocation location) {
+        // Move the marker
+        Marker marker = this.markers.get(key);
+        if (marker != null) {
+            this.animateMarkerTo(marker, location.latitude, location.longitude);
+        }
+    }
+
+    @Override
+    public void onGeoQueryReady() {
+    }
+
+    @Override
+    public void onGeoQueryError(FirebaseError error) {
+        new AlertDialog.Builder(this)
+                .setTitle("Error")
+                .setMessage("Something went wrong " + error.getMessage())
+                .setPositiveButton(android.R.string.ok, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
+
+    private void animateMarkerTo(final Marker marker, final double lat, final double lng) {
+        final Handler handler = new Handler();
+        final long start = SystemClock.uptimeMillis();
+        final long DURATION_MS = 3000;
+        final Interpolator interpolator = new AccelerateDecelerateInterpolator();
+        final LatLng startPosition = marker.getPosition();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                float elapsed = SystemClock.uptimeMillis() - start;
+                float t = elapsed/DURATION_MS;
+                float v = interpolator.getInterpolation(t);
+
+                double currentLat = (lat - startPosition.latitude) * v + startPosition.latitude;
+                double currentLng = (lng - startPosition.longitude) * v + startPosition.longitude;
+                marker.setPosition(new LatLng(currentLat, currentLng));
+
+
+                if (t < 1) {
+                    handler.postDelayed(this, 16);
+                }
+            }
+        });
+    }
+
+    private double zoomLevelToRadius(double zoomLevel) {
+
+        return 16384000/Math.pow(2, zoomLevel);
+    }
+
+    @Override
+    public void onCameraChange(CameraPosition cameraPosition) {
+
+        LatLng center = cameraPosition.target;
+        double radius = zoomLevelToRadius(cameraPosition.zoom);
+        this.searchCircle.setCenter(center);
+        this.searchCircle.setRadius(radius);
+        this.geoQuery.setCenter(new GeoLocation(center.latitude, center.longitude));
+        // radius in km
+        this.geoQuery.setRadius(radius/1000);
+    }
+}
